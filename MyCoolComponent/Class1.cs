416db3ede1cfab0d2d8f13918d90a9267b4a843c
devendra kumar <devendrakumar1007@gmail.com>
2021-01-03 21:50:09 +0530

    First commit for dotnet application

---
+++ MyCoolComponent/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoolComponent
+{
+    public class Class1
+    {
+        //some minor changes, that is compatible
+
+        // some addional change
+    }
+}
